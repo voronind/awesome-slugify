0fb7d0e58475393f7b4b9b790ebaa06b782f85ad
dimka665 <dimka665@gmail.com>
2014-03-27 23:25:39 +0400

    setup.py was refactored

--- setup.py
+++ setup.py
@@ -1,50 +1,32 @@
 # coding=utf8
 
-from setuptools import setup
-import os
-
-version = '1.2.3'
-
-name = 'awesome-slugify'
-package = 'slugify'
-description = 'Python flexible slugify module'
-url = 'https://github.com/dimka665/awesome-slugify'
-author = 'Dmitry Voronin'
-author_email = 'dimka665@gmail.com'
-license = 'GNU GPLv3'
-install_requires = [
-    'Unidecode',
-    ]
-classifiers = [
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 2',
-    'Programming Language :: Python :: 2.6',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.3',
-]
-
-
-def get_packages(package):
-    return [dirpath for dirpath, dirnames, filenames in os.walk(package)
-            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
-
+from setuptools import setup, find_packages
 
 setup(
-    name=name,
-    version=version,
+    name='awesome-slugify',
+    version='1.2.3',
 
-    url=url,
-    license=license,
-    description=description,
-    author=author,
-    author_email=author_email,
-    packages=get_packages(package),
-    py_modules=['slugify/slugify'],
-    install_requires=install_requires,
-    classifiers=classifiers,
-    keywords='slugify,alternative russian slugify, flexible, awesome',
+    author='Dmitry Voronin',
+    author_email='dimka665@gmail.com',
+
+    url='https://github.com/dimka665/awesome-slugify',
+    description='Python flexible slugify module',
+
+    packages=find_packages(),
+    install_requires='Unidecode',
+
+    license='GNU GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+    ],
+    keywords='slugify alternative russian slugify flexible awesome',
 )
+
 
 
