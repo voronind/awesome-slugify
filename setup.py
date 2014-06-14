08c0c7d6492a87e78243ee8c1527a352d150ccf8
dimka665 <dimka665@gmail.com>
2014-06-14 18:43:31 +0400

    Bumped version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='awesome-slugify',
-    version='1.4',
+    version='1.4.1',
 
     author='Dmitry Voronin',
     author_email='dimka665@gmail.com',
